aa23ec0613057c5fdabcb228a58539f1ff4340f6
Ana-María Dobre <ana-maria.dobre@comparis.ch>
2023-02-06 08:12:27 +0100

    added all libs like LSM

---
+++ .next/static/chunks/pages/index-246768ce1186f6ec.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{8312:function(n,s,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return u(5970)}])},5970:function(n,s,u){"use strict";u.r(s);var _=u(5893);let e=()=>(0,_.jsx)("main",{className:"",children:"dsadasdasd"});s.default=e}},function(n){n.O(0,[774,888,179],function(){return n(n.s=8312)}),_N_E=n.O()}]);
---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import { type NextPage } from "next";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import { useState } from "react";
+
+
+const PartnerList: NextPage = () => {
+    
+
+    return (
+        <main className="">
+           dsadasdasd
+        </main>
+    );
+};
+
+export default PartnerList;
